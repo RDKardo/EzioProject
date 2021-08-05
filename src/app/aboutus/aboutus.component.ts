810b49a3ebc0e530cbb6cbd4d68bb54b3410630f
jossuevallejo <josue.vallejo@hotmail.com>
2021-08-04 22:28:28 -0500

    #4 Remodelado aboutus

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenavContainer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-aboutus',
@@ -13,3 +14,4 @@ export class AboutusComponent implements OnInit {
   }
 
 }
+
