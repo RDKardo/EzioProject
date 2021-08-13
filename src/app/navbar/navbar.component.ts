2f546e0eaf085ef3ae765819aaae7ab065b6a8de
jossuevallejo <josue.vallejo@hotmail.com>
2021-08-12 22:43:13 -0500

    #14 Responsive design implementado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,20 +9,19 @@ export class NavbarComponent {
 
   constructor() { }
 
-  toHome(){
-    document.getElementById("home").scrollIntoView({behavior:"smooth"});
+  toHome() {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
   }
 
-  toAboutus(){
-    document.getElementById("aboutus").scrollIntoView({behavior:"smooth",block:'start'});
+  toAboutus() {
+    document.getElementById("aboutus").scrollIntoView({ behavior: "smooth", block: 'start' });
   }
 
-  toProducts(){
-    document.getElementById("products").scrollIntoView({behavior:"smooth",block:"start"});
+  toProducts() {
+    document.getElementById("products").scrollIntoView({ behavior: "smooth", block: "start" });
   }
 
-  toContact(){
-    document.getElementById("contact").scrollIntoView({behavior:"smooth"});
+  toContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
   }
-
 }
